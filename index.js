39b9ead65694dc27e48b274b13c0d5df4e88edac
MarionNafula <wekesamarion100@gmail.com>
2022-12-06 14:23:26 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+
 // Write your solution in this file!
+// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = "BOB";
+}
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "Maggie";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Meredith";
+}
+console.log(changeLeastFavoriteCustomer())
